eb98e3c524274b5d36302a1ea66f4bb13065e79c
Dennis O'Connor <doconnor@mygrocerydeals.com>
2016-01-11 12:01:21 -0500

    Server will restart if run again

--- tasks/hapi-glue.js
+++ tasks/hapi-glue.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Glue = require('glue');
 
 module.exports = function(grunt) {
+  var runningServers = {};
   grunt.registerMultiTask("hapiGlue", "Start a Hapi web server using Glue", function() {
 
     var done = this.async();
@@ -12,6 +13,7 @@ module.exports = function(grunt) {
     });
     var manifestOption = this.data.manifest;
     var glueOptions = this.data.glueOptions;
+    var target = this.target;
     var manifest;
 
     if (typeof manifestOption === 'string' || manifestOption instanceof String) {
@@ -20,23 +22,33 @@ module.exports = function(grunt) {
       manifest = manifestOption;
     }
     if (manifest) {
-      Glue.compose(manifest, glueOptions, function(err, server) {
-        if(err) {
-          grunt.fatal('Hapi server failed to start with error: ' + err);
-        }
-        server.start(function (error) {
-          var ports = server.connections.map(function(connection) {
-            return connection.info.port;
-          }).join(',');
-          grunt.log.write('Started the plot device on port(s): '+ ports + '\n');
-          if(error) {
-            grunt.fatal('Hapi server failed to start with error: ' + error);
+      var runServer = function() {
+        Glue.compose(manifest, glueOptions, function(err, server) {
+          if(err) {
+            grunt.fatal('Hapi server failed to start with error: ' + err);
           }
-          if (!options.keepAlive) {
-            done();
-          }
+          server.start(function (error) {
+            var ports = server.connections.map(function(connection) {
+              return connection.info.port;
+            }).join(',');
+            grunt.log.write('Started the plot device on port(s): '+ ports + '\n');
+            if(error) {
+              grunt.fatal('Hapi server failed to start with error: ' + error);
+            }
+            runningServers[target] = this;
+            if (!options.keepAlive) {
+              done();
+            }
+          });
         });
-      });
+      };
+      if (runningServers.hasOwnProperty(target)) {
+        runningServers[target].stop(function(err) {
+          runServer();
+        });
+      } else {
+        runServer();
+      }
       if (options.keepAlive){
         grunt.log.write("Waiting forever....\n");
       }
